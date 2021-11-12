1c9f7f327d3a27ff78669a468e4051849b6953d0
Stevenson Nelson <sonson521@gmail.com>
2021-11-12 15:15:23 -0500

    remove first paragraph

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ let displayQuote = document.getElementById('quotesHere');
 
 button.addEventListener('click', async function () {
   let res = await axios.get('https://api.tronalddump.io/random/quote');
+
   getQuote(res.data.value);
 });
 
 let getQuote = (quote) => {
   let para = document.createElement('p');
-  para.textContent = quote;
+
+  para.innerHTML = quote;
   displayQuote.appendChild(para);
+  if (displayQuote.children.length > 1) {
+    para.previousSibling.remove();
+  }
 };
+
+/*
+  I want to press the button have the first quote removed and the next quote come up. 
+*/
